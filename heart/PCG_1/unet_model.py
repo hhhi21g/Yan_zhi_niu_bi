0821b238fb42aec0d5f3f8ccb1deaca5a99b668a
hhhi21g <xuyuetong2025@163.com>
2025-04-16 10:30:25 +0800

    delete

--- heart/PCG_1/unet_model.py
+++
@@ -1,8 +0,0 @@
-from .unet_parts import *
-
-class UNet(nn.Module):
-    def __init__(self,n_channels,n_classes):
-        super(UNet,self).__init__()
-        self.n_channels = n_channels
-        self.n_classes = n_classes
-
